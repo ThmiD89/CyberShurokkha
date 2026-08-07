ca1cdb41f17a591ba9cf15c6422d99e637c0334c
Tahmid <prijeldas201@gmail.com>
2026-08-07 21:45:08 +0600

    CORS

--- backend/main.py
+++ backend/main.py
@@ -81,7 +81,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://cyber-shurokkha.onrender.com"],
+    allow_origins=["http://localhost:3000", "https://cyber-shurokkha.onrender.com" , "https://cybershurokkha-frontend.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
